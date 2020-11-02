65121b17d29ea0c23a951771b03e19a556692f20
luizacintraf <luizacintraf@gmail.com>
2020-11-01 23:32:33 -0300

    express

---
+++ projeto_DH/devhouse/models/Produto.js
@@ -0,0 +1,39 @@
+module.exports = (sequelize, DataTypes)=>{
+    const produto = sequelize.define('Produto',{
+        idprodutos:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        codigo: DataTypes.STRING,
+        nome:DataTypes.STRING,
+        preco:DataTypes.DECIMAL,
+        imagem:DataTypes.STRING,
+        quantidade: DataTypes.INTEGER,
+        tamanho:{
+            type: DataTypes.STRING,
+            allowNull:true
+        },
+       cor:{
+            type: DataTypes.STRING,
+            allowNull:true
+        },
+        hexcor:{
+            type: DataTypes.STRING,
+            allowNull:true
+        },
+        desconto:{
+            type: DataTypes.DECIMAL,
+            allowNull:true
+        },
+        id_produtos_categorias:DataTypes.INTEGER
+    },{
+        tableName:'produtos'
+    })
+
+    produto.associate = (models)=> {
+        produto.belongsTo(models.Categoria,{foreignKey:'id_produtos_categorias',as:'categorias'})
+    }
+
+    return produto
+}
