4ada5832a36d2bc6b9e29aff50572875d25b2ab2
Luiza Cintra Fernandes <luiza.fernandes@arcadis.com>
2021-02-04 09:14:40 -0300

    carrinho

--- devhouse/routes/index.js
+++ devhouse/routes/index.js
@@ -1,20 +1,21 @@
 var express = require('express');
 var router = express.Router();
-var indexController=require('../controllers/indexController')
+var indexController = require('../controllers/indexController')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HOME' });
+    res.render('index', { title: 'HOME' });
 });
 
 router.get('/produtos/:categoria?/:subcategoria?', indexController.loadAllProducts);
 router.get('/buscar', indexController.busca);
 router.get('/categoriesload', indexController.loadAllCategories);
 router.get('/produto/:codigo', indexController.loadProductById);
+router.post('/checkout', indexController.checkout);
 
 /* GET home page. */
 router.get('/checkout', function(req, res, next) {
-  res.render('carrinho', { title: 'carrinho' });
+    res.render('carrinho', { title: 'carrinho' });
 });
 
 module.exports = router;
